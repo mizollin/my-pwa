846a50afc01d9c0583c4e68a4f83d70e92fe2b01
Reto Rezzonico <r.rezzonico@gmx.ch>
2018-09-12 17:26:58 +0200

    Cache config

--- backend/app.js
+++ backend/app.js
@@ -14,20 +14,18 @@ var corsOptions = {
   //,credentials:true
 }
 
+var apiFunction = function (req, res) {
+  setTimeout(() => res.send(new Date()), 1000);
+}
+
 //here is the magic
 app.use(cors(corsOptions));
 
-app.get('/first', function (req, res) {
-  res.send(new Date());
-});
+app.get('/first', apiFunction);
 
-app.get('/second', function (req, res) {
-  res.send(new Date());
-});
+app.get('/second', apiFunction);
 
-app.get('/third', function (req, res) {
-  res.send(new Date());
-});
+app.get('/third', apiFunction);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
