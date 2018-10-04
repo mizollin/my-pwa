d50c1245ce2e6d9862c1c3665541f66389331379
Reto Rezzonico <r.rezzonico@gmx.ch>
2018-10-04 07:16:19 +0200

    Cache data calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'My Pwa!';
 
-  constructor() {
+  date$: Observable<any>;
+  secondDate$: Observable<any>;
+  thirdDate$: Observable<any>;
+
+  constructor(private http: HttpClient) {
   }
+
+  ngOnInit(): void {
+    this.refresh();
+  }
+
+  refresh() {
+    this.date$ = this.http.get('http://localhost:3000/first').pipe(
+      map((res) => res)
+    );
+
+    this.secondDate$ = this.http.get('http://localhost:3000/second').pipe(
+      map((res) => res)
+    );
+
+    this.thirdDate$ = this.http.get('http://localhost:3000/third').pipe(
+      map((res) => res)
+    );
+  }
+
 }
